90920ebfd6f471090672654f1a225f28c20c1d0f
headsht.100.beatch <headsht.100.beatch@mail.ru>
2020-04-15 02:28:01 +0400

    Добавьте файлы проекта.

---
+++ Лабораторная 3,1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Лабораторная_3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Take number");
+             string proverka = Console.ReadLine();
+            Proverka(proverka);
+            string chislo = proverka;
+            
+           
+
+                Palidrom1(chislo);
+            
+            
+            
+          
+
+        }
+
+        public static void Palidrom1(string chislo2)
+        {
+            char[] arr = chislo2.ToCharArray();
+            Array.Reverse(arr);
+            string rez = new string(arr);
+
+
+            if (chislo2 == rez)
+            {
+                Console.WriteLine("Palidrom");
+            }
+            else
+            {
+                Console.WriteLine("Error 245");
+            }
+        }
+           public static string Proverka(string proverka1)
+        {
+            if (proverka1.Length < 3)
+            {
+                Console.WriteLine("Маленькое количество символов");
+                Console.Write("Reload Call");
+                return Console.ReadLine();
+
+
+
+
+            }
+            else
+            {
+
+
+
+                
+
+               return proverka1 ;
+            }
+          
+
+
+        }
+
+    }
+
+}
+
+
+
+        
+      
+       
+    
+
